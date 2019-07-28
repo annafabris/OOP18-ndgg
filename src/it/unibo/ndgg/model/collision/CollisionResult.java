8b806849cba95ee6d6cbdd5a74594ff7e4ef2a30
Davide Lega <davide.lega2@studio.unibo.it>
2019-07-29 00:07:53 +0200

    Add CollisionResult

---
+++ src/it/unibo/ndgg/model/collision/CollisionResult.java
@@ -0,0 +1,30 @@
+package it.unibo.ndgg.model.collision;
+/**
+ * An enumeration which represent all possible outcomes of a collision from two bodies.
+ */
+public enum CollisionResult {
+    /**
+     * Represent a collision between a player and a sword that result in the player death.
+     */
+    PLAYERKILLED,
+
+    /**
+     * Represent a collision between a door and the player that is supposed to open it.
+     */
+    DOORTOUCHED,
+
+    /**
+     * Represent a collision between a player and a sword that result in the picking up of the sword.
+     */
+    SWORDPICKEDUP,
+
+    /**
+     * Represent a collision between a sword and a platform that result in the sword staying on the spot.
+     */
+    SWORDONTHEGROUND,
+
+    /**
+     * Represent a collision between a sword and a sword that result in the disarmament of a player.
+     */
+    PLAYERDISARMED;
+}
