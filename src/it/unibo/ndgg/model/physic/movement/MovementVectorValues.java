9c5e1a54f0e9897c4920079637a6648308ca0c89
Anna Fabris <anna.fabris@studio.unibo.it>
2019-07-31 16:21:31 +0200

    Delete Player and Sword MovementVectorValues, Add MovementVectorValuesImpl

--- src/it/unibo/ndgg/model/physic/movement/MovementVectorValues.java
+++ src/it/unibo/ndgg/model/physic/movement/MovementVectorValues.java
@@ -1,9 +1,19 @@
 package it.unibo.ndgg.model.physic.movement;
 
+import org.dyn4j.geometry.Vector2;
+
+import it.unibo.ndgg.model.entity.EntityMovement;
+
 /**
- * An interface implemented by {PlayerMovementVectorValues} and {@SwordMovementVectorValues}. 
- * It creates an association between a {@MovementType} and the vector of the movement
+ * An interface implemented by {@link MovementVectorValuesImpl}. 
+ * It creates an association between an {@link EntityMovement}, a {@link EntityState} and the vector of the movement {@link Vector2}
  */
 public interface MovementVectorValues {
 
+    /**
+     * Returns the value the impulse vector should have on the xx and y axes.
+     * @param currentMovement the {@link EntityMovement} to find the associated vector
+     * @return impulse as a {@link Vector2}
+     */
+    Vector2 getMovementVector(EntityMovement currentMovement);
 }
---
+++ src/it/unibo/ndgg/model/physic/movement/MovementVectorValuesImpl.java
@@ -0,0 +1,36 @@
+package it.unibo.ndgg.model.physic.movement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.dyn4j.geometry.Vector2;
+
+import it.unibo.ndgg.model.entity.EntityMovement;
+import it.unibo.ndgg.model.entity.EntityState;
+
+/**
+ * {@inheritDoc}.
+ */
+public class MovementVectorValuesImpl implements MovementVectorValues {
+
+    private static final Map<EntityMovement, Pair<EntityState, Vector2>> VALUES = new HashMap<>();
+
+    static {
+        //TODO N.B. cambiare e testare questi valori e quali movimenti ci vanno
+        VALUES.put(EntityMovement.JUMP, Pair.of(EntityState.JUMPING, new Vector2(0, 11)));
+        VALUES.put(EntityMovement.MOVE_RIGHT, Pair.of(EntityState.MOVING_RIGHT, new Vector2(1, 0)));
+        VALUES.put(EntityMovement.MOVE_LEFT, Pair.of(EntityState.MOVING_LEFT, new Vector2(-1, 0)));
+        VALUES.put(EntityMovement.DROP, Pair.of(EntityState.DROPPING, new Vector2(0, -5)));
+        VALUES.put(EntityMovement.EQUIP, Pair.of(EntityState.EQUIPPED, new Vector2(0, 5)));    //TODO vedere come implentarlo
+        VALUES.put(EntityMovement.STAY_STILL, Pair.of(EntityState.STAYING_STILL, new Vector2(0, 0))); //TODO è necessario?
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Vector2 getMovementVector(final EntityMovement currentMovement) {
+        return MovementVectorValuesImpl.VALUES.get(currentMovement).getValue();
+    }
+}
