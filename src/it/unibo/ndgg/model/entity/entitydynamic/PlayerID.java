3fcf75bdf18dd4f60197255c4f3bfe7b45bcd2ba
Riccardo Lucchi <riccardo.lucchi4@studio.unibo.it>
2019-08-23 20:59:26 +0200

    Edit Javadoc

--- src/it/unibo/ndgg/model/entity/entitydynamic/PlayerID.java
+++ src/it/unibo/ndgg/model/entity/entitydynamic/PlayerID.java
@@ -1,25 +1,30 @@
 package it.unibo.ndgg.model.entity.entitydynamic;
+
 /**
- * 
+ * An enumeration representing the player.
  */
 public enum PlayerID {
 
     /**
-     * 
+     * A {@link PlayerID} that represents the first player.
      */
     FIRST_PLAYER(0),
 
     /**
-     * 
+     * A {@link PlayerID} that represents the second player.
      */
     SECOND_PLAYER(1);
-    
+
     private final int id;
-    
-    PlayerID(final int id){
+
+    PlayerID(final int id) {
         this.id = id;
     }
-    
+
+    /**
+     * A method that returns the player ID.
+     * @return id the id of the player.
+     */
     public int getID() {
         return id;
     }
